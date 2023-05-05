63d7501bc8ad42745e3616e5064861a141088503
Aziz Abdul Yakubu <yakubuabdulaziz641@gmail.com>
2023-05-04 17:39:06 -0700

    i've imported the routes components to the App.jsx

--- README.md
+++ README.md
@@ -6,7 +6,19 @@
 2. I created a Git-Repository called portfolio.
 3. I added a README.md file.
 4. I installed some dependencies.
+
    1. React-router-dom.
    2. React-Icons.
 
-   
+5. I added a components called routes which consist of the following;
+
+   1. About.jsx
+   2. Contact.jsx
+   3. Home.jsx
+   4. Project.jsx
+
+6. I imported all the following routes in the App.jsx file;
+   1. Route for Home page.
+   2. Route for About page.
+   3. Route for Contact page.
+   4. Route for Project page.
--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,6 +1,21 @@
 import './index.css';
+import About from './routes/About';
+import Home from './routes/Home';
+import Contact from './routes/Contact';
+import Project from './routes/Project';
+import { Route, Routes } from 'react-router-dom';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<About />} />
+        <Route path="/" element={<Contact />} />
+        <Route path="/" element={<Project />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
